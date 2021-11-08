ef9eb966df9c3410295b1881f532a309bd0f1c1d
Michael Erdely <mike@erdelynet.com>
2021-11-08 14:10:19 -0500

    changes

--- python_scripts/set_state.py
+++ python_scripts/set_state.py
@@ -1,7 +1,20 @@
 entity_id = data.get('entity_id')
 if not entity_id:
     logger.error('No entity_id provided')
+
 state = data.get('state')
 if not state:
     logger.error('No state provided')
-hass.states.set(entity_id, state)
+
+attr_change = data.get('attributes')
+
+entity = hass.states.get(entity_id)
+attributes = entity.attributes.copy()
+
+try:
+    for key in attr_change:
+        attributes[key] = attr_change[key]
+except:
+    pass
+
+hass.states.set(entity_id, state, attributes)
