2f07e0679cafae28666f7233b0b27ea88bd9905a
Michael Erdely <mike@erdelynet.com>
2023-06-30 14:53:41 -0400

    changes

--- bin/opnsense_packages.sh
+++ bin/opnsense_packages.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+if [ -z "$2" ]; then
+  eval $($(dirname "$(realpath "$0")")/opnsense_snmp_versions.py | jq  -r | awk -F'"' '$2=="core-abi"||$2=="sys-abi"{printf "%s=%s\n",gensub(/-/,"_","g",$2),$4}')
+else
+  sys_abi=$1
+  core_abi=$2
+fi
+
+#echo sys_abi=$sys_abi >> /dev/stderr
+#echo core_abi=$core_abi >> /dev/stderr
+
 tmpfile=$(mktemp)
 # Get mimugmail repo info
 cp /dev/null $tmpfile
-if [ -n "$1" ]; then
-  http_status_code=$(curl -w %{http_code} -sfo $tmpfile "https://opn-repo.routerperformance.net/repo/$1/packagesite.txz")
+if [ -n "$sys_abi" ]; then
+  http_status_code=$(curl -w %{http_code} -sfo $tmpfile "https://opn-repo.routerperformance.net/repo/$sys_abi/packagesite.txz")
   tmpdir=$(mktemp -d)
   tar -C $tmpdir -xf $tmpfile
   mimugmail=$(cat $tmpdir/packagesite.yaml|jq -r .|awk '/^  "name":/{n=$2;if(substr(n,2,3)!="os-")n=""}/^  "version":/&&n!=""{printf "    %s: %s\n",substr(n,1,length(n)-1),$2}')
@@ -13,8 +23,8 @@ fi
 
 # Get opnsense repo info
 cp /dev/null $tmpfile
-if [ -n "$2" ]; then
-  status="http_status_code=$(curl -w %{http_code} -sfo $tmpfile "https://pkg.opnsense.org/$1/$2/latest/All/"); sys_abi=$1; core_abi=$2"
+if [ -n "$core_abi" ]; then
+  status="http_status_code=$(curl -w %{http_code} -sfo $tmpfile "https://pkg.opnsense.org/$sys_abi/$core_abi/latest/All/"); sys_abi=$sys_abi; core_abi=$core_abi"
 else
   status="command usage: ${0##*/} opnsense_sys_abi opnsense_core_abi"
 fi
